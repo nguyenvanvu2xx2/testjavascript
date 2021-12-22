6829d7806ceb99b049a56dd67f46b6ac13390ca1
nguyenvanvu2xx2 <82077375+nguyenvanvu2xx2@users.noreply.github.com>
2021-12-22 14:39:28 +0700

    Release letgo

---
+++ vinh.js
@@ -0,0 +1,19 @@
+document.getElementById("open").onclick = function () {
+    document.getElementById("backgroud").style.background = 'rgb(21, 255, 0)';
+};
+
+document.getElementById("close").onclick = function () {
+    document.getElementById("backgroud").style.background = 'rgb(255, 0, 0)';
+};
+document.getElementById("white").onclick = function () {
+    document.getElementById("backgroud").style.background = 'rgb(255, 255, 255)';
+};
+
+
+document.getElementById("noi-dung").onclick = function () {
+    document.getElementById("sub").style.display = 'block';
+};
+document.getElementById("cut").onclick = function () {
+    document.getElementById("sub").style.display = 'none';
+};
+
